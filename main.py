4787bb5837e4f5e0c458cc0c0b77f94d9b6fd097
Rumyana <mijiturka@tuta.io>
2021-02-20 18:17:26 +0000

    Display several random images to create the illusion of shuffling
    Not a very nice illusion. Just an initial attempt.
    
    Add some fluff to the output prompt as well

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import os
 import random
 from PIL import Image
+import time
+
+print(1)
+time.sleep(0.1)
+print(2)
+time.sleep(0.1)
+print(3)
+time.sleep(0.1)
+print('Очаквам да знаеш за Екзюпари')
+time.sleep(1)
 
 posters_path = './movies'
 
 movies = os.listdir(posters_path)
-tonight_we_watch = random.choice(movies)
 
-poster = Image.open(os.path.join(posters_path, tonight_we_watch))
-poster.show()
+for i in range(1, 5):
+    tonight_we_watch = random.choice(movies)
+    poster = Image.open(os.path.join(posters_path, tonight_we_watch))
+    poster.show()
+    time.sleep(0.1)
 
-print(tonight_we_watch)
+# print(tonight_we_watch)
