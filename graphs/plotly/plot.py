0db55f706576ad68982a1bd41fcec8118ec2400b
Rumyana <mijiturka@tuta.io>
2021-03-27 21:06:12 +0000

    Add binary chart with fun quality+quantity
    Quality represented by point either at 1 or -1.
    Quantity represented by size of bubble around the point.
    
    These work well for displaying all the data we have
    (based on Turbulence 3 and Bigfoot Country so far)
    and they can be combined with Shmentina and Capellyana
    on the same graph (yet to complete the code for this).
    
    y is not an accurate representation of time ...yet

--- graphs/plotly/plot.py
+++ graphs/plotly/plot.py
@@ -4,16 +4,38 @@ import logging
 
 import argparse
 
-import plotly.express as px
+import plotly.express
 
 def from_report(path):
     return json.loads(path.read_text())
 
 def is_positive(value):
+    if not value:
+        return False
     return value.count('+') == len(value)
 
 def is_negative(value):
+    if not value:
+        return False
     return value.count('-') == len(value)
+
+def line_chart(timestamps, fun):
+    return plotly.express.line(x=timestamps, y=fun)
+
+def binary_chart(timestams, fun_quality, fun_quantity):
+    fig = plotly.graph_objects.Figure()
+    fig.add_trace(
+        plotly.graph_objects.Scatter(
+            x=timestamps,
+            y=fun_quality,
+            mode='markers',
+            marker=dict(
+                size=fun_quantity
+            ),
+            name='Capellyana'
+        )
+    )
+    return fig
 
 if __name__ == '__main__':
 
@@ -34,19 +56,43 @@ if __name__ == '__main__':
     elif args.file is not None:
         path = pathlib.Path(args.file)
 
+
+    # Get the data
     data = from_report(path)
 
-    timestamps = data['fun'].keys()
+    # Massage it to make it more graphable
+    timestamps = []
     fun = []
+    fun_quality = []
+    fun_quantity = []
+    # This helps visibilty, but depends on how trigger-happy we've been throughout the movie
+    multiplier = 10
 
-    for value in data['fun'].values():
+    for (timestamp, value) in data['fun'].items():
         quantity = len(value)
         if is_positive(value):
+            # Determines where this value would go on the plot
+            fun_quality.append(1)
+            # Determines size of the representation of this value
+            fun_quantity.append(quantity*multiplier)
+            # All-in-one value, useful for line charts
             fun.append(quantity)
+            timestamps.append(timestamp)
         elif is_negative(value):
+            fun_quality.append(-1)
+            fun_quantity.append(quantity*multiplier)
             fun.append(-quantity)
+            timestamps.append(timestamp)
         else:
-            logging.warning(f'Ignoring value {value}')
+            # The record is invalid. We don't want its value or timestamp
+            logging.warning(f'Ignoring value {value} at {timestamp}')
 
-    fig = px.scatter(x=timestamps, y=fun)
+
+    # Prepare the plot
+
+    # fig = line_chart(timestamps, fun)
+
+    fig = binary_chart(timestamps, fun_quality, fun_quantity)
+
+    # Start up a server and serve this one page; display in browser window
     fig.show()
